6d5b6012e21cdd6ab71e1cf9452be0654a514db1
carlos.seijo <cseijoc23@fpcoruna.afundacion.org>
2024-10-01 14:37:57 +0200

    Crear archivo operaciones.py

---
+++ sprint0python/operaciones.py
@@ -0,0 +1,31 @@
+
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "No es posible la divison"
+
+
+
+
+print(suma(2, 3))
+print(resta(5,20))
+print(multiplicacion(4,5))
+print(division(2,0))
+print(division(50,5))
+
+print("Suma:")
+a = int(input("Numero 1: "))
+b = int(input("Numero 2: "))
+print(suma(a,b))
+
+
