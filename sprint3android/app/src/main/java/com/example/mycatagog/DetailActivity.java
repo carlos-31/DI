eaadded2f7cfa13a047874075e7c7f1cf941f3cb
carlos-31 <cseijoc23@fpcoruna.afundacion.org>
2024-11-20 09:55:04 +0100

    Agregar clase DetailActivity, y botón en catalog que lleva a esa actividad

--- sprint3android/app/src/main/java/com/example/mycatagog/DetailActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatagog/DetailActivity.java
@@ -1,10 +1,6 @@
 package com.example.mycatagog;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,30 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class CatalogActivity extends AppCompatActivity {
-    Context context = this;
-
+public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_catalog);
+        setContentView(R.layout.activity_detail);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-
-
-        Button button = findViewById(R.id.buttonCatalog);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent myIntent = new Intent(context, DetailActivity.class);
-                context.startActivity(myIntent);
-            }
         });
     }
 }
